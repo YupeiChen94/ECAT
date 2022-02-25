5501260d884ef89956cf914631a1e37766afd9a4
YupeiChen <ychen@my.centenary.edu>
2022-02-24 19:18:45 -0600

    Minor update

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 import dash
-import dash_table
+from dash import dash_table
 import dash_daq as daq
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc as dcc
+from dash import html as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from dash_extensions import Download
